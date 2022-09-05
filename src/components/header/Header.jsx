103763698e3bfe7cbeca6f3c241c31fa103a9664
Henrique Rosa <samuel_kuik@hotmail.com>
2022-09-05 11:20:43 -0300

    Finalizado o Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <h1 className="title">Meu Portifólio</h1>
+        <div className="links">
+          <Link className="btn btn-primary" to="/about">Sobre</Link>
+          <Link className="btn btn-primary" to="/projects">Projetos</Link>
+          <Link className="btn btn-primary" to= "/contacts">Contatos</Link>
+        </div>
+      </header>
+    )
+  }
+}
