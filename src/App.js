25f9036a904bbff9d9004fd9d89e16b6e96b2753
Henrique Rosa <samuel_kuik@hotmail.com>
2022-09-07 11:52:48 -0300

    css responsivo e paginas em construção

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { Route, Switch } from 'react-router-dom';
+import AboutMe from './pages/About-me';
+import Contacts from './pages/Contacts';
 import Homepage from './pages/Homepage';
+import Projects from './pages/Projects';
 
 function App () {
   return (
     <Switch>
+      <Route path='/about' component={AboutMe}/>
+      <Route path='/projects' component={Projects} />
+      <Route path='/contacts' component={Contacts} />
       <Route path='/' component={Homepage}/>
     </Switch>
   )
